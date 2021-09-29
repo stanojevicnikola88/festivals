6c0e8202e66a2b6071adf4871ee8a2cbaf0da631
Nikola <stanojevic.nikola88@gmail.com>
2021-09-29 15:19:12 +0200

    Event model improvements
    Getters for date parsing init

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
 class Event extends Model
 {
@@ -22,6 +23,11 @@ class Event extends Model
         'description'
     ];
 
+    protected $appends = [
+        'date_start',
+        'date_end'
+    ];
+
     public function users(): \Illuminate\Database\Eloquent\Relations\BelongsToMany
     {
         return $this->belongsToMany(User::class);
@@ -35,5 +41,15 @@ class Event extends Model
     public function city(): \Illuminate\Database\Eloquent\Relations\BelongsTo
     {
         return $this->belongsTo(City::class);
+    }
+
+    public function getDateStartAttribute(): string
+    {
+        return Carbon::parse($this->start)->format('Y-m-d\TH:i');
+    }
+
+    public function getDateEndAttribute(): string
+    {
+        return Carbon::parse($this->end)->format('Y-m-d\TH:i');
     }
 }
