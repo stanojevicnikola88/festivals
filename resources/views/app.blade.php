b50828600937909daf5e7e6a6d8354a31eb0a9bc
Nikola <stanojevic.nikola88@gmail.com>
2021-09-30 10:02:04 +0200

    App blade and css fix

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,7 +10,6 @@
 
     <!-- Custom fonts for this template-->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.1/css/all.min.css" />
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
@@ -22,6 +21,9 @@
 <main>
     @yield('content')
 </main>
+
+<!-- MODALS -->
+@yield('modals')
 
 <!-- Scripts -->
 <script src="{{ asset('js/app.js') }}" defer></script>
