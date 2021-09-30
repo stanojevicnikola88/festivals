2be318b60d72e34da5c3ce3f94fde54972c5a1e2
Nikola <stanojevic.nikola88@gmail.com>
2021-09-30 13:44:01 +0200

    Seeders improvements

--- database/seeders/EventSeeder.php
+++ database/seeders/EventSeeder.php
@@ -21,11 +21,13 @@ class EventSeeder extends Seeder
                 'end' => '2022-07-13 06:00:00',
                 'country_id' => 1,
                 'city_id' => 1,
-                'address' => 'Petrovaradinska bb',
-                'latitude' => '123',
-                'longitude' => '456',
-                'featured_image' => 'exit.jpg',
-                'description' => 'Exit is a summer music festival which is held at the Petrovaradin Fortress in Novi Sad, Serbia. Founded in 2000, it has twice won the Best Major Festival award at the European Festivals Awards, for 2013 and 2017.'
+                'address' => 'Petrovaradinska tvrđava, Beogradska, Petrovaradin, Serbia',
+                'latitude' => '45.2512686',
+                'longitude' => '19.8617464',
+                'featured_image' => null,
+                'description' => 'Exit is a summer music festival which is held at the Petrovaradin Fortress in Novi Sad, Serbia. Founded in 2000, it has twice won the Best Major Festival award at the European Festivals Awards, for 2013 and 2017.',
+                'created_at' => '2021-09-28 18:56:54',
+                'updated_at' => '2021-09-28 18:56:54'
             ],
             [
                 'title' => 'Volt Festival',
@@ -33,11 +35,13 @@ class EventSeeder extends Seeder
                 'end' => '2022-08-13 03:00:00',
                 'country_id' => 2,
                 'city_id' => 5,
-                'address' => 'Vienna street 12',
-                'latitude' => '789',
-                'longitude' => '654',
-                'featured_image' => 'volt.jpg',
-                'description' => 'VOLT festival is a multi-genre festival in Vienna Austria. Getting its title from the pop culture magazine of the same name, the festival has been running...'
+                'address' => 'Vienna, Austria',
+                'latitude' => '48.2081743',
+                'longitude' => '16.3738189',
+                'featured_image' => null,
+                'description' => 'VOLT festival is a multi-genre festival in Vienna Austria. Getting its title from the pop culture magazine of the same name, the festival has been running...',
+                'created_at' => '2021-09-28 18:56:54',
+                'updated_at' => '2021-09-28 18:56:54'
             ],
         ]);
     }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -21,35 +21,45 @@ class UserSeeder extends Seeder
                 'last_name' => 'Doe',
                 'email' => 'admin@admin.com',
                 'password' => Hash::make('password'),
-                'is_admin' => 1
+                'is_admin' => 1,
+                'created_at' => '2021-09-28 18:56:54',
+                'updated_at' => '2021-09-28 18:56:54'
             ],
             [
                 'first_name' => 'Mark',
                 'last_name' => 'Twain',
                 'email' => 'mark@gmail.com',
                 'password' => Hash::make('password'),
-                'is_admin' => 0
+                'is_admin' => 0,
+                'created_at' => '2021-09-28 18:56:54',
+                'updated_at' => '2021-09-28 18:56:54'
             ],
             [
                 'first_name' => 'John',
                 'last_name' => 'Silver',
                 'email' => 'john@gmail.com',
                 'password' => Hash::make('password'),
-                'is_admin' => 0
+                'is_admin' => 0,
+                'created_at' => '2021-09-28 18:56:54',
+                'updated_at' => '2021-09-28 18:56:54'
             ],
             [
                 'first_name' => 'Tom',
                 'last_name' => 'Golden',
                 'email' => 'tom@gmail.com',
                 'password' => Hash::make('password'),
-                'is_admin' => 0
+                'is_admin' => 0,
+                'created_at' => '2021-09-28 18:56:54',
+                'updated_at' => '2021-09-28 18:56:54'
             ],
             [
                 'first_name' => 'Alex',
                 'last_name' => 'Ward',
                 'email' => 'alex@gmail.com',
                 'password' => Hash::make('password'),
-                'is_admin' => 0
+                'is_admin' => 0,
+                'created_at' => '2021-09-28 18:56:54',
+                'updated_at' => '2021-09-28 18:56:54'
             ]
         ]);
     }
